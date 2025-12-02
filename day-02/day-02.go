57c6477388cd33a4c9662a05a365bc92114a9909
Michael Josephson <30024+mikej@users.noreply.github.com>
2025-12-02 09:53:31 +0000

    preparation for part 2
    add function to split into n equal parts and then check if all parts are
    the same. for now just call this with n = 2 to match the behaviour for
    part 1.

--- day-02/day-02.go
+++ day-02/day-02.go
@@ -42,6 +42,37 @@ func isInvalid(i int64) bool {
 		return false
 	}
 
-	midpoint := len(str) / 2
-	return str[:midpoint] == str[midpoint:]
+	parts, _ := parts(str, 2)
+	return allSame(parts)
+}
+
+func parts(str string, n int) ([]string, error) {
+	totalLength := len(str)
+
+	if totalLength%n != 0 {
+		return nil, fmt.Errorf("string length %d is not divisible by %d", totalLength, n)
+	}
+
+	partLength := totalLength / n
+
+	var parts = make([]string, n)
+	for i := 0; i < n; i++ {
+		parts[i] = str[(i * partLength) : (i*partLength)+partLength]
+	}
+
+	return parts, nil
+}
+
+func allSame(s []string) bool {
+	if len(s) == 0 {
+		return true
+	}
+
+	for i := 1; i < len(s); i++ {
+		if s[i] != s[0] {
+			return false
+		}
+	}
+
+	return true
 }
