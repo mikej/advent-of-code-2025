064ea65a68c9eea107d1e0f52bf477577381a390
Michael Josephson <30024+mikej@users.noreply.github.com>
2025-12-03 17:13:09 +0000

    update to use part 2 maxJoltage for part 1 as well
    we’ve now got a more general purpose function that accepts a number of batteries to turn on so we can go back to part 1 and use this function instead of the function that was hardcoded to use 2 batteries/digits.

--- day-03/day-03.go
+++ day-03/day-03.go
@@ -18,47 +18,31 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	total := 0
+	part1Total := 0
+	part2Total := 0
+
 	for scanner.Scan() {
 		line := scanner.Text()
-		joltageForLine, err := maxJoltage2(line, 12)
+
+		joltageForLine, err := maxJoltage2(line, 2)
 		if err != nil {
 			fmt.Println("Error getting joltage for line", err)
 		}
-		total += joltageForLine
+		part1Total += joltageForLine
+
+		joltageForLine, err = maxJoltage2(line, 12)
+		if err != nil {
+			fmt.Println("Error getting joltage for line", err)
+		}
+		part2Total += joltageForLine
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading file", err)
 	}
 
-	fmt.Println("Total joltage is", total)
-}
-
-func maxJoltage(line string) (int, error) {
-	if len(line) < 2 {
-		return 0, fmt.Errorf("need at least 2 digits")
-	}
-
-	var maxFirstDigit = string(line[0])
-	var maxIndex = 0
-
-	for i := 1; i < len(line)-1; i++ {
-		if string(line[i]) > maxFirstDigit {
-			maxFirstDigit = string(line[i])
-			maxIndex = i
-		}
-	}
-
-	var maxSecondDigit = string(line[maxIndex+1])
-	for i := maxIndex + 1; i < len(line); i++ {
-		if string(line[i]) > maxSecondDigit {
-			maxSecondDigit = string(line[i])
-		}
-	}
-
-	number, _ := strconv.Atoi(maxFirstDigit + maxSecondDigit)
-	return number, nil
+	fmt.Println("Part 1 total joltage is", part1Total)
+	fmt.Println("Part 2 total joltage is", part2Total)
 }
 
 func maxJoltage2(line string, digitsToUse int) (int, error) {
--- day-03/day-03_test.go
+++ day-03/day-03_test.go
@@ -3,42 +3,42 @@ package main
 import "testing"
 
 func TestMaxJoltageWhereDigitsAreTogether(t *testing.T) {
-	checkMaxJoltage(t, "987654321111111", 98)
+	checkMaxJoltageForPart1(t, "987654321111111", 98)
 }
 
 func TestMaxJoltageWhereDigitsAreAtEachEnd(t *testing.T) {
-	checkMaxJoltage(t, "811111111111119", 89)
+	checkMaxJoltageForPart1(t, "811111111111119", 89)
 }
 
 func TestMaxJoltageWhereDigitsAreSeparatedPartWayThrough(t *testing.T) {
-	checkMaxJoltage(t, "818181911112111", 92)
+	checkMaxJoltageForPart1(t, "818181911112111", 92)
 }
 
 func TestMaxJoltageReturnsAnErrorWhenStringIsTooShort(t *testing.T) {
-	result, err := maxJoltage("1")
+	result, err := maxJoltage2("1", 2)
 	if err == nil {
 		t.Errorf("expected to get an error, got no error and a result of %d", result)
 	}
 }
 
-func TestMaxJoltage2LeavingSome1sAtTheEnd(t *testing.T) {
-	checkMaxJoltage2(t, "987654321111111", 987654321111)
+func TestMaxJoltageForPart2LeavingSome1sAtTheEnd(t *testing.T) {
+	checkMaxJoltageForPart2(t, "987654321111111", 987654321111)
 }
 
-func TestMaxJoltage2LeavingSome1sInTheMiddle(t *testing.T) {
-	checkMaxJoltage2(t, "234234234234278", 434234234278)
+func TestMaxJoltageForpart2LeavingSome1sInTheMiddle(t *testing.T) {
+	checkMaxJoltageForPart2(t, "234234234234278", 434234234278)
 }
 
-func TestMaxJoltage2LeavingSomeLowDigits(t *testing.T) {
-	checkMaxJoltage2(t, "234234234234278", 434234234278)
+func TestMaxJoltageForPart2LeavingSomeLowDigits(t *testing.T) {
+	checkMaxJoltageForPart2(t, "234234234234278", 434234234278)
 }
 
-func TestMaxJoltage2LeavingSome1sNearTheFront(t *testing.T) {
-	checkMaxJoltage2(t, "818181911112111", 888911112111)
+func TestMaxJoltageForPart2LeavingSome1sNearTheFront(t *testing.T) {
+	checkMaxJoltageForPart2(t, "818181911112111", 888911112111)
 }
 
-func checkMaxJoltage(t *testing.T, str string, expected int) {
-	result, err := maxJoltage(str)
+func checkMaxJoltageForPart1(t *testing.T, str string, expected int) {
+	result, err := maxJoltage2(str, 2)
 	if err != nil {
 		t.Errorf("unexpected error getting max joltage: %v", err)
 	}
@@ -47,7 +47,7 @@ func checkMaxJoltage(t *testing.T, str string, expected int) {
 	}
 }
 
-func checkMaxJoltage2(t *testing.T, str string, expected int) {
+func checkMaxJoltageForPart2(t *testing.T, str string, expected int) {
 	result, err := maxJoltage2(str, 12)
 	if err != nil {
 		t.Errorf("unexpected error getting max joltage: %v", err)
