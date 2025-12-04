4e3f59c38ef73fcfad4d5acc72fb98b369c5ab1e
Michael Josephson <30024+mikej@users.noreply.github.com>
2025-12-04 13:48:46 +0000

    day 4 part 1

---
+++ day-04/day-04.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mikej/advent-of-code-2025/shared/input"
+)
+
+func main() {
+	lines, err := input.ReadFromFile("/Users/mike/Downloads/input-day-4.txt")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	grid := make([][]bool, len(lines))
+	for i := range lines {
+		grid[i] = make([]bool, len(lines[i]))
+	}
+
+	for i := range lines {
+		for j := range lines[i] {
+			grid[i][j] = string(lines[i][j]) == "@"
+		}
+	}
+
+	accessibleRolls := 0
+	for i := range grid {
+		for j := range grid[i] {
+			if grid[i][j] && countNeighboours(grid, i, j) < 4 {
+				accessibleRolls++
+			}
+		}
+	}
+
+	fmt.Println(accessibleRolls)
+}
+
+func countNeighboours(grid [][]bool, x, y int) int {
+	count := 0
+
+	for i := x - 1; i <= x+1; i++ {
+		for j := y - 1; j <= y+1; j++ {
+			if (i == x && j == y) || i < 0 || j < 0 || i >= len(grid) || j >= len(grid[i]) {
+				continue
+			}
+			if grid[i][j] {
+				count++
+			}
+		}
+	}
+
+	return count
+}
