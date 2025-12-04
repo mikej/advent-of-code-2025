f86925eb53bbf7bdde82f83aa1a4aeff5a55998f
Michael Josephson <30024+mikej@users.noreply.github.com>
2025-12-04 13:48:39 +0000

    add helper to read lines from an input file

--- shared/input/input.go
+++ shared/input/input.go
@@ -1,7 +1,28 @@
 package input
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func ReadFromFile(filename string) ([]string, error) {
-	return nil, fmt.Errorf("not implemented")
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("error opening file: %w", err)
+	}
+	defer file.Close()
+
+	var lines []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error reading file: %w", err)
+	}
+
+	return lines, nil
 }
