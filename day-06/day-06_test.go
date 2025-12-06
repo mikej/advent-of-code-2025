e06f80c8654fa396dfb7b997366e42ee21fdabdf
Michael Josephson <30024+mikej@users.noreply.github.com>
2025-12-06 17:57:09 +0000

    add test case for day 6 part 1

--- day-06/day-06_test.go
+++ day-06/day-06_test.go
@@ -1,1 +1,17 @@
 package main
+
+import "testing"
+
+func TestPart1(t *testing.T) {
+	input := []string{
+		"123 328 51  64",
+		"45  64  387 23",
+		"6   98  215 314",
+		"*   +   *   +  ",
+	}
+
+	result := SolvePart1(input)
+	if result != 4277556 {
+		t.Errorf("Expected 4277556, got %d", result)
+	}
+}
